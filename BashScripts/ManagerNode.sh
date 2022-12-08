239c8dc994f0c888c049825b59593b83eb7e4794
Anoohya <Anoohya@gmail.com>
2022-12-07 23:40:41 -0500

    Fixed web page path

--- BashScripts/ManagerNode.sh
+++ BashScripts/ManagerNode.sh
@@ -15,10 +15,10 @@ sudo touch /etc/nginx/conf.d/static-naice-me.conf
 # start writing configuration for nginx
 sudo echo 'server {' > /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  server_name pcvm2-9.instageni.utdallas.edu;' >> /etc/nginx/conf.d/static-naice-me.conf
-sudo echo '  root '`pwd`'/CS655FinalProject/Web;' >> /etc/nginx/conf.d/static-naice-me.conf
+sudo echo '  root '`pwd`'/CS655Final-main/Web;' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  index index.html;' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  location ~* ^.+\.(jpg|jpeg|gif|png|ico|css|js|pdf|txt){' >> /etc/nginx/conf.d/static-naice-me.conf
-sudo echo '    root '`pwd`'/CS655FinalProject/Web;' >> /etc/nginx/conf.d/static-naice-me.conf
+sudo echo '    root '`pwd`'/CS655Final-main/Web;' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  }' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '}' >> /etc/nginx/conf.d/static-naice-me.conf
 

661c858e8fee7d13370ae67e6a7c2e2565d49bad
Anoohya <Anoohya@gmail.com>
2022-12-07 21:15:17 -0500

    Fixed problem of neme of tar file

--- BashScripts/ManagerNode.sh
+++ BashScripts/ManagerNode.sh
@@ -9,14 +9,14 @@ sudo apt install nginx
 # download project files.
 sudo wget --no-check-certificate --no-cache --no-cookies https://github.com/anoohya29/CS655Final/blob/e4eeaac062f88740ad57e8c3aaf27786cd537ecc/CS655FinalProject.tar.zip --post-data="action=purge"
 # extract project files
-sudo tar -zvxf password_cracker_v1.0.tar.gz
+sudo tar -zvxf CS655FinalProject.tar.gz
 # create the configuration file for nginx
 sudo touch /etc/nginx/conf.d/static-naice-me.conf
 # start writing configuration for nginx
 sudo echo 'server {' > /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  server_name pcvm3-8.instageni.cenic.net;' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  root '`pwd`'/CS655FinalProject/Web;' >> /etc/nginx/conf.d/static-naice-me.conf
-sudo echo '  index homepage.html;' >> /etc/nginx/conf.d/static-naice-me.conf
+sudo echo '  index index.html;' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  location ~* ^.+\.(jpg|jpeg|gif|png|ico|css|js|pdf|txt){' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '    root '`pwd`'/CS655FinalProject/Web;' >> /etc/nginx/conf.d/static-naice-me.conf
 sudo echo '  }' >> /etc/nginx/conf.d/static-naice-me.conf
--- BashScripts/WorkerNode.sh
+++ BashScripts/WorkerNode.sh
@@ -7,7 +7,7 @@ sudo apt install default-jdk
 # download project files.
 sudo wget --no-check-certificate --no-cache --no-cookies https://github.com/anoohya29/CS655Final/blob/e4eeaac062f88740ad57e8c3aaf27786cd537ecc/CS655FinalProject.tar.zip --post-data="action=purge"
 # extract project files
-sudo tar -zvxf password_cracker_v1.0.tar.gz 
+sudo tar -zvxf CS655FinalProject.tar.gz
 # Change directory to worker folder.
 cd CS655FinalProject/WorkerNode  # go into the worker node folder
 # complie the java files
